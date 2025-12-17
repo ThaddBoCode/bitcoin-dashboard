5e4f8487c48f6b5ad573819314180d765f72ad18
ThaddBoCode <161933181+ThaddBoCode@users.noreply.github.com>
2025-12-17 12:10:25 +0100

    Add ADDRESS_BOOK object with contact details

---
+++ config.js
@@ -0,0 +1,7 @@
+    const ADDRESS_BOOK = {
+      "bc1qpl73fwdsankdjz0tcetmndfdjgadr54tqxtya0": "Juna",
+      "bc1qqt7d5zm9sejaak8j2x2jgcqrxjg2hzj5ydl00e": "Louisa",
+      "bc1q5g38zwqaw665epm8ftvddrumnetv0graxls5x5": "Fiona",
+      "bc1qzfq978e2rd78d4snyj8mzdtwlcplxzl677daz6": "Emile",
+      "bc1q9tsv5047vm40ht9a3prgk78x4x7z09f3pj3a6u": "Oscar",
+};
