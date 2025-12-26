8a44c16fa9ca0a76d0fcdd7dfc81a39ff553b007
ThaddBoCode <161933181+ThaddBoCode@users.noreply.github.com>
2025-12-26 21:22:20 +0100

    Update ADDRESS_BOOK entries in config.js
    Removed several entries from the ADDRESS_BOOK object and added two new entries.

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
     const ADDRESS_BOOK = {
-      "bc1qpl73fwdsankdjz0tcetmndfdjgadr54tqxtya0": "Juna",
-      "bc1qqt7d5zm9sejaak8j2x2jgcqrxjg2hzj5ydl00e": "Louisa",
-      "bc1q5g38zwqaw665epm8ftvddrumnetv0graxls5x5": "Fiona",
-      "bc1qzfq978e2rd78d4snyj8mzdtwlcplxzl677daz6": "Emile",
-      "bc1q9tsv5047vm40ht9a3prgk78x4x7z09f3pj3a6u": "Oscar",
-      "bc1q5lr0s25kcft9qdvyfgqgguv2qrexhr4pskm20j": "Ramon",        
+      "bc1q2p5n2ete0zexwwqxeuqes3esgect5a9tn0gxlp": "Louisa",
+      "bc1qwxenjray28uqg6dz7dln5s67lrgyrt9e2eexvx": "Fiona"
 };
